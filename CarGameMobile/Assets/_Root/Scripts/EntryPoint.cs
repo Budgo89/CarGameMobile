b887ddfb8a6fec1ce8ca7542f5fefbd2a5f5f948
LAPTOP-PSEPRFOJ\Максим <budgo@mail.ru>
2022-02-26 13:07:37 +0300

    start

---
+++ CarGameMobile/Assets/_Root/Scripts/EntryPoint.cs
@@ -0,0 +1,44 @@
+using Profile;
+using UnityEngine;
+using Services.IAP;
+using Services.Analytics;
+using Services.Ads.UnityAds;
+
+internal class EntryPoint : MonoBehaviour
+{
+    private const float SpeedCar = 15f;
+    private const GameState InitialState = GameState.Start;
+
+    [SerializeField] private Transform _placeForUi;
+    [SerializeField] private IAPService _iapService;
+    [SerializeField] private UnityAdsService _adsService;
+    [SerializeField] private AnalyticsManager _analytics;
+
+    private MainController _mainController;
+
+
+    private void Awake()
+    {
+        var profilePlayer = new ProfilePlayer(SpeedCar, InitialState);
+        _mainController = new MainController(_placeForUi, profilePlayer);
+
+        if (_adsService.IsInitialized) OnAdsInitialized();
+        else _adsService.Initialized.AddListener(OnAdsInitialized);
+
+        if (_iapService.IsInitialized) OnIapInitialized();
+        else _iapService.Initialized.AddListener(OnIapInitialized);
+
+        _analytics.SendMainMenuOpened();
+    }
+
+    private void OnDestroy()
+    {
+        _adsService.Initialized.RemoveListener(OnAdsInitialized);
+        _iapService.Initialized.RemoveListener(OnIapInitialized);
+        _mainController.Dispose();
+    }
+
+
+    private void OnAdsInitialized() => _adsService.InterstitialPlayer.Play();
+    private void OnIapInitialized() => _iapService.Buy("product_1");
+}
