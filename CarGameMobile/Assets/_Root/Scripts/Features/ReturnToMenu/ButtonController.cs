025cc043dc85a7ae4a7759fa2cc5c69312180d1f
LAPTOP-PSEPRFOJ\Максим <budgo@mail.ru>
2022-04-06 01:06:45 +0300

    Задание 1 доп файлы

---
+++ CarGameMobile/Assets/_Root/Scripts/Features/ReturnToMenu/ButtonController.cs
@@ -0,0 +1,38 @@
+﻿using Features.Fight;
+using Profile;
+using System;
+using Tool;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Features.ReturnToMenu
+{
+    internal class ButtonController : BaseController
+    {
+        private readonly ResourcePath _resourcePath = new ResourcePath("Prefabs/Fight/ReturnToMenuView");
+
+        private readonly ReturnToMenuView _view;
+        private readonly ProfilePlayer _profilePlayer;
+
+
+        public ButtonController(Transform placeForUi, ProfilePlayer profilePlayer)
+        {
+            _profilePlayer = profilePlayer;
+            _view = LoadView(placeForUi);
+            _view.Init(Return);
+        }
+        
+
+        private ReturnToMenuView LoadView(Transform placeForUi)
+        {
+            GameObject prefab = ResourcesLoader.LoadPrefab(_resourcePath);
+            GameObject objectView = Object.Instantiate(prefab, placeForUi, false);
+            AddGameObject(objectView);
+
+            return objectView.GetComponent<ReturnToMenuView>();
+        }
+
+        private void Return() =>
+            _profilePlayer.CurrentState.Value = GameState.Start;
+    }
+}
---
+++ CarGameMobile/Assets/_Root/Scripts/Features/ReturnToMenu/ReturnToMenuView.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace Features.ReturnToMenu
+{
+    internal class ReturnToMenuView : MonoBehaviour
+    {
+        [SerializeField] private Button _returnButton;
+
+        public void Init(UnityAction returnMainMenu) =>
+            _returnButton.onClick.AddListener(returnMainMenu);
+
+        private void OnDestroy() =>
+            _returnButton.onClick.RemoveAllListeners();
+    }
+}
