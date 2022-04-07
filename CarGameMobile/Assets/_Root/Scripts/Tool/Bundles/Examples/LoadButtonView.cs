e90bdb1f3025627233bacbecf7bf6464cfbdd307
LAPTOP-PSEPRFOJ\Максим <budgo@mail.ru>
2022-04-07 22:48:43 +0300

    Задание 2, ну судите строго, аврал(

--- CarGameMobile/Assets/_Root/Scripts/Tool/Bundles/Examples/LoadButtonView.cs
+++ CarGameMobile/Assets/_Root/Scripts/Tool/Bundles/Examples/LoadButtonView.cs
@@ -11,24 +11,50 @@ namespace Tool.Bundles.Examples
         [Header("Asset Bundles")]
         [SerializeField] private Button _loadAssetsButton;
 
+        [Header("Addressables")]
+        [SerializeField] private AssetReference _setPicrureAssetReference;
+        [SerializeField] private Image _image;
+        [SerializeField] private Button _setPictureButton;
+        [SerializeField] private Button _deletePictureButton;
+
+        private Sprite _addressable;
         private GameObject _addressablePrefab;
 
         private void Start()
         {
             _loadAssetsButton.onClick.AddListener(LoadAsset);
+            _setPictureButton.onClick.AddListener(SetPicture);
+            _deletePictureButton.onClick.AddListener(DeletePicture);
+        }
+
+        private void SetPicture()
+        {
+            _addressable =
+                Addressables.LoadAssetAsync<Sprite>(_setPicrureAssetReference).Result;
+            _image.sprite = _addressable;
+        }
+
+        private void DeletePicture()
+        {
+            _image.sprite = null;
+            Addressables.Release(_addressable);
+        }
+
+
+
+        private void OnDestroy()
+        {
+            _loadAssetsButton.onClick.RemoveAllListeners();
+            _setPictureButton.onClick.RemoveAllListeners();
+            _deletePictureButton.onClick.RemoveAllListeners();
+
+            DespawnPrefabs();
         }
 
         private void LoadAsset()
         {
             _loadAssetsButton.interactable = false;
             StartCoroutine(DownloadAndSetAssetBundles());
-        }
-
-        private void OnDestroy()
-        {
-            _loadAssetsButton.onClick.RemoveAllListeners();
-
-            DespawnPrefabs();
         }
 
         private void DespawnPrefabs()
